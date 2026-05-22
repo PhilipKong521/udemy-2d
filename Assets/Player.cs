0ce66bacfec7b336acebf0d8f5364851322f40eb
PhilipKong521 <kongxiangpo@gmail.com>
2026-05-22 13:15:23 +0800

    编写了跳跃方法

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,10 +6,17 @@ public class Player : MonoBehaviour
     [SerializeField] private Animator anim;
     [SerializeField] private Rigidbody2D rb;
 
+    [Header("MoveMent details")]
     [SerializeField] private float xInput;
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float jumpForce = 8f;
-    [SerializeField] private bool facingRight = true;
+                             private bool facingRight = true;
+
+    [Header("Collision details")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+                             private bool isGrounded;
+
 
 
     private void Awake()
@@ -19,6 +26,7 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
+        HandleCollision();
         HandleInput();
         HandleMovement();
         HandleAnimation();
@@ -41,16 +49,26 @@ public class Player : MonoBehaviour
             Jump();
     }
 
+    //移动
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
     }
 
+    // 跳跃方法
     private void Jump()
     {
+        if(isGrounded)
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 
+    //检测是否在地面
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    //人物反转
     private void HandleFlip()
     {
         if (rb.linearVelocity.x > 0 && facingRight == true)
@@ -58,9 +76,16 @@ public class Player : MonoBehaviour
         else if (rb.linearVelocity.x < 0 && facingRight == false)
             Flip();
     }
+
     private void Flip()
     {
         transform.Rotate (0, 180, 0);
         facingRight = !facingRight;
+    }
+
+    //使用OnDrawGizmos延Y轴向下画一条线
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
     }
 }
